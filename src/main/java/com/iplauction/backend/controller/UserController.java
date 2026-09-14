ee0ec16973e8c8ec9a07c876da9938fc888943c7
rbsun <sundarajan4702@gmail.com>
2026-09-14 18:17:05 +0530

    feat: update authorities in user controller

--- src/main/java/com/iplauction/backend/controller/UserController.java
+++ src/main/java/com/iplauction/backend/controller/UserController.java
@@ -1,21 +1,31 @@
 package com.iplauction.backend.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
 public class UserController {
+
     @GetMapping("/api/me")
     public Map<String, Object> getCurrentUser(
-            @AuthenticationPrincipal OAuth2User user) {
+            @AuthenticationPrincipal OAuth2User user,
+            Authentication authentication) {
+
+        List<String> authorities = authentication.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
 
         return Map.of(
                 "attributes", user.getAttributes(),
-                "authorities", user.getAuthorities()
+                "authorities", authorities
         );
     }
 }
