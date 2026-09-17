9916de01ab7497924ed93066abfb46438c26df81
rbsun <sundarajan4702@gmail.com>
2026-09-17 08:35:12 +0530

    feat: update cors config for allowed frontend urls

--- src/main/java/com/iplauction/backend/config/CorsConfig.java
+++ src/main/java/com/iplauction/backend/config/CorsConfig.java
@@ -17,7 +17,8 @@ public class CorsConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of(
-                "http://localhost:4200"
+                "http://localhost:4200",
+                "https://ipl-auction-frontend-ten.vercel.app"
         ));
 
         configuration.setAllowedMethods(List.of(
